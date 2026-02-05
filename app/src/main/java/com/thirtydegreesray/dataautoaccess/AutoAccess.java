5ebec9e899be4387ff05d6af177c4c11f462cad9
kuake0 <78269501+kuake0@users.noreply.github.com>
2026-02-04 16:01:16 -0800

    Add stub AutoAccess annotation to allow build without DataAutoAccess library
    Added a stub annotation for compatibility purposes.

---
+++ app/src/main/java/com/thirtydegreesray/dataautoaccess/AutoAccess.java
@@ -0,0 +1,14 @@
+package com.thirtydegreesray.dataautoaccess;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Stub AutoAccess annotation for compatibility.
+ */
+@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.FIELD})
+public @interface AutoAccess {
+}
